77a1bd727dfa293d518cf5d145c65e6b0346780b
antonin <ironmac@mbp-de-antonin.home>
2014-04-13 21:05:38 +0200

    Add new JS files for the game

---
+++ JS/lightCycle.js
@@ -0,0 +1,132 @@
+/*
+    lightCycle class
+    Programmer: Carette Antonin
+    Date: 04/13/2014
+ */
+
+/*
+    lightCycle is an object who represent a lightCycle in the Tron's Universe.
+    A LightCycle is represented with his name.
+    @param name: The name of the object lightCycle
+    @param x: The position of the X axis
+    @param y: The position of the Y axis
+    @param direction: The direction of the lightCycle object
+    @param color: The color of the lightCycle
+ */
+function LightCycle(name, x, y, direction, color) {
+    this.name = name;
+    this.positionXOrigins = x;
+    this.positionYOrigins = y;
+    this.currentX = x;
+    this.currentY = y;
+    this.directionOrigins = direction;
+    this.currentDirection = direction;
+    this.color = color;
+    this.height = 8;
+    this.width = 8;
+    this.speed = 2;
+    this.score = 0;
+    this.coordinates = [];
+    
+    /*
+     Function which be able to reset all the modified parameters of a lightCycle object
+     */
+    this.reset = function() {
+        this.currentX = this.positionXOrigins;
+        this.currentY = this.positionYOrigins;
+        this.direction = this.directionOrigins;
+        this.coordinates = [];
+    };
+    
+    /*
+     Function which be able to generate the coordinates of a lightCycle object
+     */
+    this.generateCoordinates = function() {
+        return this.currentX+","+this.currentY;
+    };
+    
+    /*
+     Function which be able to get the array of coordinates from a lightCycle object
+     */
+    this.getCoordinates = function() {
+        return this.coordinates;
+    };
+    
+    /*
+     Function which be able to push coordinates into the coordinates's array of a lightCycle object
+     */
+    this.pushCoordinates = function() {
+        this.coordinates.push(this.generateCoordinates());
+    };
+    
+    /*
+     Function which be able to set the speed of the lightCycle
+     */
+    this.setSpeed = function(newSpeed) {
+        this.speed = newSpeed;
+    };
+    
+    /*
+     Function which be able to increment the score of the player
+     */
+    this.incrementScore = function() {
+        this.score += 1;
+    };
+    
+    /*
+     Function which be able to calculating new coordinates of the lightCycle object
+     */
+    this.calculatingCoordinates = function() {
+        switch (this.direction) {
+            case "left":
+                this.currentX -= this.width;
+                break;
+            case "right":
+                this.currentX += this.width;
+                break;
+            case "up":
+                this.currentY += this.height;
+                break;
+            case "down":
+                this.currentY -= this.height;
+                break;
+        }
+    };
+    
+    /*
+     Render of the lightCycle
+     */
+    this.render = function() {
+        content.fillStyle = this.color;
+        content.beginPath();
+        content.moveTo(this.currentX - (this.width / 2), this.currentY - (this.height / 2));
+        content.lineTo(this.currentX + (this.width / 2), this.currentY - (this.height / 2));
+        content.lineTo(this.currentX + (this.width / 2), this.currentY + (this.height / 2));
+        content.lineTo(this.currentX - (this.width / 2), this.currentY + (this.height / 2));
+        content.closePath();
+        content.fill();
+    };
+    
+    /*
+     Check if there are collisions or not
+     */
+    this.collision = function(player) {
+        if ((this.currentX < (this.width / 2)) ||
+            (this.currentY < (this.height / 2)) ||
+            (this.currentX > (Grid.width - (this.width / 2))) ||
+            (this.currentY > (Grid.height - (this.height / 2))) ||
+            player.getCoordinates().indexOf(this.generateCoordinates()) >= 0) /*
+            || this.getCoordinates().indexOf(this.generateCoordinates()) >=0)                                                                   */
+            return true;
+        return false;
+    }
+    
+}
+
+/*
+ move is a function which be able to the lightCycle to move on the grid
+ */
+LightCycle.prototype.move = function() {
+    this.calculatingCoordinates();
+    this.pushCoordinates();
+};
---
+++ JS/main.js
@@ -0,0 +1,221 @@
+/*
+ Main class
+ Programmer: Carette Antonin
+ Date: 04/13/2014
+ */
+var canvas = document.getElementById("myCanvas");
+var content = canvas.getContext("2d");
+var height = canvas.height;
+var width = canvas.width;
+content.fillRect(0,0,width,height);
+content.clearRect(0,0,width,height);
+
+/*
+ Counting the number of time we have fun!
+ */
+var count_grid = 0;
+/*
+ Boolean to know if we are be able to continue to play, or not
+ */
+var continuePlay = true;
+
+/*
+ Coordinates of the player and the AI
+ */
+var xProgram = width - (width / 4);
+var yProgram = height - (height / 2);
+var xProgrammer = width - (3 * width / 4);
+var yProgrammer = height - (height / 2);
+
+incrementCountGrid = function() {
+    count_grid+=1;
+};
+
+/*
+ Two objects LightCycles: the player (Programmer) and the AI (Program)
+ */
+var program = new LightCycle("Program", xProgram, yProgram, "left", "orange");
+var programmer = new LightCycle("Programmer", xProgrammer, yProgrammer, "right", "blue");
+
+/*
+ List of keys from keyboards available
+ */
+var keys = {
+    left : [37,81],
+    up: [40,83],
+    right: [39,68],
+    down: [38,90]
+};
+
+oppositeDirection = function(direction) {
+    switch(direction) {
+            case "up":
+                return "down";
+                break;
+            case "down":
+                return "up";
+                break;
+            case "left":
+                return "right";
+                break;
+            case "right":
+                return "left";
+                break;
+    }
+}
+
+/*
+ Initialization function
+ */
+init = function() {
+    Grid.start();
+    setInterval(play, 100);
+};
+
+/*
+ Principale function of the game
+ */
+play = function() {
+    if (continuePlay) {
+        programmer.move();
+        programmer.render();
+        if (programmer.collision(program)) Grid.renew(program);
+        programmer.pushCoordinates();
+        program.move();
+        program.pushCoordinates();
+        program.render();
+        if (program.collision(programmer)) Grid.renew(programmer);
+        program.pushCoordinates();
+        //Grid.getConsoleLogStart();
+    }
+    else {
+        Grid.finish();
+    }
+};
+
+/*
+ Grid object - the "board"
+ */
+Grid = {
+    
+    height: canvas.height,
+    width: canvas.width,
+    
+    checkRenew: function() {
+        incrementCountGrid();
+        if (count_grid >= 5) {
+            continuePlay = false;
+        }
+    },
+    
+    renew: function(player) {
+        player.score += 1;
+        Grid.reset();
+        Grid.checkRenew();
+        programmer.reset();
+        program.reset();
+    },
+    
+    getConsoleLogStart: function() {
+        console.log(program.name+": "+program.score);
+        console.log(programmer.name+": "+programmer.score);
+    },
+    
+    getColorWinner: function(winner) {
+        return winner.color;
+    },
+    
+    resetCountGrid: function() {
+        count_grid = 0;
+    },
+    
+    resetPlayers: function() {
+        program.reset();
+        programmer.reset();
+    },
+    
+    start: function() {
+        Grid.reset();
+        //Grid.getContentTextStart();
+        Grid.resetCountGrid();
+        Grid.resetPlayers();
+    },
+    
+    returnWinner: function() {
+        if (program.score > programmer.score) return program;
+        if (program.score < programmer.score) return programmer;
+        else return null;
+    },
+    
+    finish: function() {
+        Grid.getConsoleLogFinish(Grid.returnWinner());
+    },
+    
+    getRectTextFont: function() {
+        content.font = '25px sans-serif';
+        content.textAlign = 'center';
+    },
+    
+    getScore: function() {
+        content.fillText(programmer.name+':'+programmer.score+' / '+program.name+':'+program.score, width/2, height/2 + 30);
+    },
+    
+    
+    getContentTextStart: function(){
+        Grid.getRectTextFont();
+        content.fillText('WELCOME!', width/2, height/3);
+        content.fillText('You\'re the player to the left, don\'t forget it!',width/2, height/2);
+    },
+    
+    getContentTextFinish: function(winner) {
+        Grid.getRectTextFont();
+        content.fillText('Score: ', width/2, height/2.5);
+        content.fillText(winner.name+' is the winner! :-)', width/2, height/2);
+        Grid.getScore();
+    },
+    
+    getTextFinishNull: function() {
+        content.fillText('NO WINNER', width/2, height/3);
+        Grid.getScore();
+    },
+    
+    getConsoleLogFinish: function(winner) {
+        if (winner != null) {
+            content.fillStyle = Grid.getColorWinner(winner);
+            Grid.reset();
+            Grid.getContentTextFinish(winner);
+        }
+        else {
+            content.fillStyle = "black";
+            Grid.reset();
+            Grid.getTextFinishNull();
+        }
+    },
+    
+    reset:function(){
+        content.clearRect(0,0,width,height);
+    },
+    
+};
+
+/*
+ To get the direction give by the player
+ */
+Object.prototype.getKey = function(value) {
+    for (var key in this) {
+        if(this[key] instanceof Array && this[key].indexOf(value) >= 0) return key;
+    }
+    return null;
+};
+
+/*
+ To do the action with the current direction
+ */
+addEventListener("keydown", function(k) {
+                 var newDirection = keys.getKey(k.keyCode);
+                 if (programmer.direction != oppositeDirection(newDirection)) programmer.direction = newDirection;
+                 }, false);
+
+window.onload = function() {
+    init();
+};
